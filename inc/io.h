8d35e0ac5b88d2dc6c3efa4225b9a84b07e3e01b
Calebe Rios <calebe_rios@hotmail.com>
2017-08-15 13:41:57 +0000

    Exclusão algumas pastas

--- inc/io.h
+++
@@ -1,5 +0,0 @@
-/*Utilize um arquivo .c/.h com as funções de entrada e saída*/
-
-void ler_pontos(double *pontos);
-void mostrar_pontos(double raio, double *centro);
-void circ_n_viavel();
--- inc/operations.h
+++
@@ -1,6 +0,0 @@
-/*Utilize um arquivo .c/.h para realizar as operações geométricas/algébricas
-necessárias para o cálculo do raio e do centro do círculo.*/
-
-void calc_raio(double *pontos);
-void calc_centro(double *pontos);
-int verificar_poli(double *pontos);
